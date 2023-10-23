c9b9a889dc698c9a0edaa85e7da087cbb159d101
fanagiannis <foulis67@gmail.com>
2023-10-23 17:26:09 +0300

    init colors fps display

--- Template.py
+++ Template.py
@@ -1,7 +1,27 @@
 import pygame
+import sys
 from pygame.locals import * 
 
 pygame.init()
 
+#COLORS
+color_black=pygame.color(0,0,0)
+color_white=pygame.color(255,255,255)
+color_grey=pygame.color(128,128,128)
+color_red=pygame.color(255,0,0)
+
+#DISPLAY WINDOW
+window_size= pygame.display.set_mode((300,300))
+
+#FPS 
+game_fps=pygame.time.clock()
+game_fps.tick(60)
+
+while True:
+    for event in pygame.event.get():
+        if event.type()==QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
 
     
