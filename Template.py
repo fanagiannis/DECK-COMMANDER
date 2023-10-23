51122af54d9204b8e51c679436ac5c677ac63426
fanagiannis <foulis67@gmail.com>
2023-10-23 16:47:05 +0300

    pygame init

--- Template.py
+++ Template.py
@@ -1,27 +1,7 @@
 import pygame
-
 from pygame.locals import * 
-from sys import exit
 
 pygame.init()
 
-screen = pygame.display.set_mode((1240,720))
-clock=pygame.time.Clock()
-running=True
 
-while running:
-
-    for event in pygame.event.get():
-        if event.type == pygame.quit:
-            running=False
-    
-    screen.fill("purple")
-
-    pygame.display.flip()
-
-    clock.tick(60)
-    inp=input("Quit? ")
-    if inp=="y":
-        running=False 
-pygame.quit()
     
