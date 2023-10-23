36b53558004036212b223c15d3621e323226f3bc
fanagiannis <foulis67@gmail.com>
2023-10-23 21:26:55 +0300

    0.0.2

--- Template.py
+++ Template.py
@@ -2,15 +2,15 @@ import pygame
 import sys
 import random
 from pygame.locals import *
-from pygame.sprite import _Group 
+from pygame.sprite import Group 
 
 pygame.init()
 
 #COLORS
-color_black=pygame.color(0,0,0)
-color_white=pygame.color(255,255,255)
-color_grey=pygame.color(128,128,128)
-color_red=pygame.color(255,0,0)
+color_black=pygame.Color(0,0,0)
+color_white=pygame.Color(255,255,255)
+color_grey=pygame.Color(128,128,128)
+color_red=pygame.Color(255,0,0)
 
 #DISPLAY WINDOW
 display_width=400
@@ -20,11 +20,11 @@ display_window.fill(color_white)
 pygame.display.set_caption("Test")
 
 #FPS 
-game_fps=pygame.time.clock()
+game_fps=pygame.time.Clock()
 
 #OBJECTS
-object1=pygame.rect((20,50),(50,100))
-object2=pygame.rect((10,10),(100,100))
+#object1=pygame.rect((20,50),(50,100))
+#object2=pygame.rect((10,10),(100,100))
 
 #CLASSES
 
@@ -34,7 +34,7 @@ class Enemy(pygame.sprite.Sprite):
     enemy_speed=10
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("D:\ΦΩΤΟ\SHREKORLOS")
+        self.image = pygame.image.load("D:\\ΦΩΤΟ\\SHREKORLOS.png")
         self.rect=self.image.get_rect()
         self.rect.center=(random.randint(40,display_width-40),0) #randomiser
     def movement(self):
@@ -50,10 +50,12 @@ class Enemy(pygame.sprite.Sprite):
 
 class Player(pygame.sprite.Sprite):
     global player_speed
-    player_speed=5
+    global player_quit
+    player_speed=50
+    player_quit=False
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("D:\ΦΩΤΟ\mqdefault__1_-removebg-preview")
+        self.image = pygame.image.load("D:\\ΦΩΤΟ\\mqdefault__1_-removebg-preview.png")
         self.rect=self.image.get_rect()
         self.rect.center=(160,520)
 
@@ -61,14 +63,18 @@ class Player(pygame.sprite.Sprite):
 
     def movement(self):
         pressed_keys=pygame.key.get_pressed()
-        if pressed_keys[K_RIGHT]:
-            self.rect.move_ip(player_speed,0) #movecmd
-        if pressed_keys[K_LEFT]:
-            self.rect.move_ip(-player_speed,0)
-        if pressed_keys[K_UP]:
+        if pressed_keys[K_w]:
             self.rect.move_ip(0,-player_speed)
-        if pressed_keys[K_DOWN]:
+        if pressed_keys[K_s]:
             self.rect.move_ip(0,player_speed)
+        if self.rect.right< display_width:
+            if pressed_keys[K_d]:
+                self.rect.move_ip(player_speed,0) #movecmd
+        if self.rect.left>0:
+            if pressed_keys[K_a]:
+                self.rect.move_ip(-player_speed,0)
+        
+        
 
     #PLAYER_SPAWN
 
@@ -81,18 +87,19 @@ E=Enemy()
 
 while True:
     for event in pygame.event.get():
-        if event.type()==QUIT:
+        if event.type==QUIT:
             pygame.quit()
-            sys.exit()
+            sys.exit(0)
 
     #INITIALIZE
 
-    P.update()
+    P.movement()
     E.movement()
     
     #BACKGROUND
 
     display_window.fill(color_white)
+
 
     #SPAWN
 
