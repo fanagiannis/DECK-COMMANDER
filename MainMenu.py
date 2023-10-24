e376ce2417c47ed5fe6b4e3de2906c42671bbe01
fanagiannis <foulis67@gmail.com>
2023-10-24 21:57:01 +0300

    rename

--- MainMenu.py
+++ MainMenu.py
@@ -3,4 +3,11 @@ import pygame_menu
 import sys
 import random
 import time
+from pygame.locals import *
 
+pygame.init()
+
+while True:
+  for event in pygame.event.get():
+    if event.type == QUIT :
+      exit()
