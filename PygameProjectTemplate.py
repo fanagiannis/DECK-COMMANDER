1fd32d7d001e12c52f760aab57ad3479774943d1
fanagiannis <foulis67@gmail.com>
2023-10-24 22:05:11 +0300

    Start

--- PygameProjectTemplate.py
+++ PygameProjectTemplate.py
@@ -7,6 +7,21 @@ from pygame.locals import *
 
 pygame.init()
 
+#COLORS
+color_black=pygame.Color(0,0,0)
+color_white=pygame.Color(255,255,255)
+color_grey=pygame.Color(128,128,128)
+color_red=pygame.Color(255,0,0)
+
+#DISPLAY WINDOW
+display_width=600
+display_height=400
+display_window=pygame.display.set_mode((display_width,display_height))
+
+#FPS 
+game_fps=pygame.time.Clock()
+FPS=60
+
 while True:
   for event in pygame.event.get():
     if event.type == QUIT :
