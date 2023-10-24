a3034aef030926ebcdffff0cbe67d181a6bd6fb1
fanagiannis <foulis67@gmail.com>
2023-10-24 22:13:34 +0300

    0.0.0.1

--- MainMenu.py
+++ MainMenu.py
@@ -22,7 +22,30 @@ display_window=pygame.display.set_mode((display_width,display_height))
 game_fps=pygame.time.Clock()
 FPS=60
 
+#FUNCTIONS
+
+def set_difficulty(stage,difficulty):
+  print("difficulty:1")
+
+def game_start():
+  print("Game Started ! ")
+
+#DIFFICULTY 
+
+
+
+#MENU
+
+menu=pygame_menu.Menu("Main Menu",400,300,theme=pygame_menu.themes.THEME_BLUE)
+
+menu.add.text_input(" Username : ",default="Uknown Player")
+menu.add.selector(" Difficulty : ",[("Hard",1),("Easy",2)],onchange=set_difficulty)
+menu.add.button(" Start Game ",game_start)
+menu.add.button(" Quit ",pygame_menu.events.EXIT)
+
 while True:
   for event in pygame.event.get():
     if event.type == QUIT :
       exit()
+
+  menu.mainloop(display_window)
