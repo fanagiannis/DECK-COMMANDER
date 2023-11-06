7d73e0da5847588d61ce2c671a9c047f7be91897
fanagiannis <foulis67@gmail.com>
2023-11-06 14:16:32 +0200

    small fixes

--- Variables.py
+++ Variables.py
@@ -22,10 +22,10 @@ Target_spawn=Spawner(60)
 Target_Damage=250
 
 #global score_message_pos,lives_message_pos,game_over_message_pos,ammo_message_pos, ammo_no_message_pos
-score_message_pos=(30,DISPLAY_HEIGHT-50)
-lives_message_pos=(DISPLAY_WIDTH-150,DISPLAY_HEIGHT-35)
+score_message_pos=(30,DISPLAY_HEIGHT-60)
+lives_message_pos=(DISPLAY_WIDTH-175,DISPLAY_HEIGHT-35)
 game_over_message_pos=(DISPLAY_WIDTH/2-75,DISPLAY_HEIGHT/2-75)
-ammo_message_pos=(DISPLAY_WIDTH-150,DISPLAY_HEIGHT-75)
+ammo_message_pos=(DISPLAY_WIDTH-175,DISPLAY_HEIGHT-75)
 ammo_no_message_pos=(DISPLAY_WIDTH-150,DISPLAY_HEIGHT-150)
 
 score_value=100
