0a41e63264d105ae4c31e07339a6e0910e9d8ec8
fanagiannis <foulis67@gmail.com>
2023-11-06 10:32:31 +0200

    DB EDIT

--- Database/CreateDB.py
+++ Database/CreateDB.py
@@ -10,5 +10,25 @@ def create_connection(path):
         print(f"Error '{e}' occured!")
     return connection
 
+def query_execution(connection,query):
+    cursor=connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Executed!")
+    except Error as e:
+        print(f"Error '{e}' occured!")
+        pass
+
 connection=create_connection("deckcommander_db.sql")
 
+query_create_table="""
+CREATE TABLE IF NOT EXISTS players (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+username TEXT NOT NULL,
+score INTEGER,
+highscore INTEGER 
+);
+"""
+
+query_execution(connection,query_create_table)
